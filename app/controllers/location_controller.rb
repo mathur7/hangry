8541d4da434feee5cc166f7bc34b69db9f16de4b
mathur7 <mathur7@gmail.com>
2014-09-08 11:42:18 -0700

    changed api query

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,15 +1,18 @@
 class LocationController < ApplicationController
 
   def index
+
   end
 
   def results 
-    parameters = { term: 'restaurant', lat:params[:lat],long:params[:long], limit: 1 }
-    @response = render json: Yelp.client.search('San Francisco', parameters)
-    @response.inspect
-    # @address = response.businesses[0].location.display_address[0]
-    # binding.pry
-    # @name = response.businesses[0].name
+    parameters = { term: 'restaurants', lat:params[:lat],long:params[:long], limit: 1 }
+    response = Yelp.client.search('San Francisco', parameters)
+    @address = response.businesses[0].location.display_address[0]
+    @name = response.businesses[0].name
+    @rating = response.businesses[0].rating
+    @rating_image = response.businesses[0].rating_img_url_small
+    @phone = response.businesses[0].phone
+    @image = response.businesses[0].image_url
     # @lat =  response.businesses[0].location.coordinate.latitude
     # @long = response.businesses[0].location.coordinate.longitude
   end
